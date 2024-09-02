98d2d3c2ce499d0ccc0c6bbaf816aa5eae7137e0
Thanuga-Rodrigo <144921624+Thanuga-Rodrigo@users.noreply.github.com>
2024-09-02 09:59:17 +0530

    did changes in pickup box

--- lib/screens/pick_box_page.dart
+++ lib/screens/pick_box_page.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gifts/common_widgets/submit_button_widget.dart';
+import 'package:gifts/utils/constants.dart';
 
 class PickBoxPage extends StatefulWidget {
   const PickBoxPage({super.key});
@@ -12,6 +15,7 @@ class PickBoxPage extends StatefulWidget {
 class _PickBoxPageState extends State<PickBoxPage> {
   int _countdown = 10;
   late Timer _timer;
+  bool? isFound = false;
 
   @override
   void initState() {
@@ -20,7 +24,7 @@ class _PickBoxPageState extends State<PickBoxPage> {
   }
 
   void _startCountdown() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       if (_countdown > 0) {
         setState(() {
           _countdown--;
@@ -36,6 +40,7 @@ class _PickBoxPageState extends State<PickBoxPage> {
     _timer.cancel();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
@@ -48,40 +53,55 @@ class _PickBoxPageState extends State<PickBoxPage> {
               children: [
                 ClipPath(
                   clipper: TopCircleClipper(),
-                  child: Container(
-                    height: 300,
-                    color: colorScheme.primary
-                  ),
+                  child: Container(height: 300, color: colorScheme.primary),
                 ),
-                Padding(padding: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Center(child: Text('Pick your lucky box', style: textTheme.headlineLarge?.copyWith(color: colorScheme.onPrimaryFixed, fontSize: 28, fontWeight: FontWeight.w600),)),
-                    SizedBox(height: 32,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('You picked sunglasses',style: textTheme.titleLarge?.copyWith(color: colorScheme.onPrimaryFixed,)),
-                        SizedBox(width: 12,),
-                        Container(
-                          height: 26,
-                          width: 26,
-                          decoration: BoxDecoration(
-                            color: Colors.black,
-                            shape: BoxShape.circle
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 90,),
-                    Container(
-                      height: 130,
-                      width: 130,
-                      decoration: BoxDecoration(
-                        color: colorScheme.onPrimaryFixed,
-                        shape: BoxShape.circle
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Center(
+                          child: Text(
+                        isFound == null
+                            ? 'Pick your lucky box'
+                            : isFound!
+                                ? 'You Found it!'
+                                : 'Better Luck Next Time!',
+                        style: textTheme.headlineLarge?.copyWith(
+                            color: colorScheme.onPrimaryFixed,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600),
+                      )),
+                      const SizedBox(
+                        height: 32,
                       ),
-                      child: Center(
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('You picked sunglasses',
+                              style: textTheme.titleLarge?.copyWith(
+                                color: colorScheme.onPrimaryFixed,
+                              )),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          Container(
+                            height: 26,
+                            width: 26,
+                            decoration: const BoxDecoration(
+                                color: Colors.black, shape: BoxShape.circle),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 90,
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                            color: colorScheme.onPrimaryFixed,
+                            shape: BoxShape.circle),
+                        child: Center(
                           child: Stack(
                             alignment: Alignment.center,
                             children: [
@@ -89,14 +109,14 @@ class _PickBoxPageState extends State<PickBoxPage> {
                                 height: 76,
                                 width: 76,
                                 child: CircularProgressIndicator(
-                                  value: _countdown / 10,
+                                  value: isFound == null ? _countdown / 10 : 0,
                                   strokeWidth: 5,
                                   backgroundColor: Colors.grey.shade300,
                                   color: Colors.greenAccent,
                                 ),
                               ),
                               Text(
-                                '$_countdown',
+                                isFound == null ? '$_countdown' : '00',
                                 style: textTheme.headlineLarge?.copyWith(
                                   fontWeight: FontWeight.w700,
                                 ),
@@ -104,15 +124,157 @@ class _PickBoxPageState extends State<PickBoxPage> {
                             ],
                           ),
                         ),
-                    ),
-                    SizedBox(height: 48,),
-                    Row(
-                      children: [
-                        
+                      ),
+                      const SizedBox(
+                        height: 48,
+                      ),
+                      if (isFound == null) ...[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/gift_icon.svg',
+                              height: 118,
+                              width: 112,
+                            ),
+                            const SizedBox(
+                              width: 50,
+                            ),
+                            SvgPicture.asset(
+                              'assets/icons/gift_icon.svg',
+                              height: 118,
+                              width: 112,
+                            ),
+                          ],
+                        ),
+                      ] else if (isFound == true) ...[
+                        SvgPicture.asset(
+                          'assets/icons/found_box.svg',
+                          height: 184,
+                          width: 166,
+                        ),
+                      ] else if (isFound == false) ...[
+                        SvgPicture.asset(
+                          'assets/icons/failed_box.svg',
+                          height: 185,
+                          width: 166,
+                        ),
                       ],
-                    )
-                  ],
-                ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Box',
+                            style: textTheme.headlineMedium?.copyWith(
+                                fontSize: 22, fontWeight: FontWeight.w600),
+                          ),
+                          Text(
+                            ' 1 ',
+                            style: textTheme.headlineMedium?.copyWith(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w600,
+                                color: colorScheme.primary),
+                          ),
+                          Text(
+                            'of 20',
+                            style: textTheme.headlineMedium?.copyWith(
+                                fontSize: 22, fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: colorScheme.onTertiary,
+                        ),
+                        child: const Center(
+                          child: Text('Test Ad'),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      if (isFound == null) ...[
+                        Container(
+                          height: 48,
+                          width: 133,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: colorScheme.onTertiary,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Next',
+                              style: textTheme.headlineMedium
+                                  ?.copyWith(color: colorScheme.tertiary),
+                            ),
+                          ),
+                        )
+                      ] else if (isFound == true) ...[
+                        const SubmitButtonWidget(
+                            buttonHeight: 48,
+                            buttonWidth: 133,
+                            text: 'Next',
+                            hasIcon: false,
+                            buttonIconUrl: ''),
+                      ] else if (isFound == false) ...[
+                        Container(
+                          height: 48,
+                          width: 133,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: LightThemeAppColors.starColour,
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Retry',
+                              style: textTheme.headlineMedium
+                                  ?.copyWith(color: colorScheme.onPrimaryFixed),
+                            ),
+                          ),
+                        )
+                      ],
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          // Generate dots
+                          for (int i = 0; i < 19; i++)
+                            Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 4.0),
+                              width: 8.0,
+                              height: 8.0,
+                              decoration: BoxDecoration(
+                                color: i == 0
+                                    ? colorScheme
+                                        .primary // Fully pink for the first dot
+                                    : colorScheme.primary
+                                        .withOpacity(0.3), // Adjust opacity
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          // Add gift icon at the end
+                          Container(
+                            margin: const EdgeInsets.only(left: 8.0),
+                            child: SvgPicture.asset(
+                              'assets/icons/gift_small.svg',
+                              height: 40,
+                              width: 40,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 )
               ],
             )
@@ -122,7 +284,6 @@ class _PickBoxPageState extends State<PickBoxPage> {
     );
   }
 }
-
 
 class TopCircleClipper extends CustomClipper<Path> {
   @override
